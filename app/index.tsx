73142cf9ab2ab5cc49727be75028cdb678f97b7b
Anatolii Halas <tolikgalas@gmail.com>
2024-05-11 16:43:40 +0300

    (Fixes)FE-1-Dependencies-and-Config/ Fixed nativewind types by adding app.d.ts into project

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="">
       <Text>Open up index.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
